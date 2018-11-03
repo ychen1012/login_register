f14a9befdefa58e64b9baf0bf7c201e0d278eac6
bsz <iamshubo@qq.com>
2018-11-03 21:59:41 +0800

    推下试试

---
+++ app/src/main/java/diary/beautiful/sync/SyncButton.java
@@ -0,0 +1,20 @@
+package diary.beautiful.sync;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+public class SyncButton extends android.support.v7.widget.AppCompatButton {
+
+    public SyncButton(Context context) {
+        super(context);
+    }
+
+    public SyncButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SyncButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
